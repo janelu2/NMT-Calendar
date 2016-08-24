101d37f48944c7a988a5798b4f2878a82c9482ca
janelu2 <lattaeo@gmail.com>
2016-08-24 17:28:41 -0600

    Accidentally deleted one too many lines, added it back in

--- src/main.py
+++ src/main.py
@@ -19,14 +19,15 @@ def find_date(event_link):
     date_info = str(div.text).split(' ', 1)[1]
     return date_info
 
-
 # this is a temporary way to loop and create a bunch of event objects stored in an array
 events = []
+calendar_soup = create_soup('http://www.nmt.edu/nmt-calendar')
 for row in calendar_soup.find_all('div', attrs={"class": "eventmiddle"}):
     event_link = "http://www.nmt.edu" + str(row.a['href'])
     new_event = NMTevent(row, event_link, find_date(event_link))
     events.append(new_event)
-    new_event.pretty()  # pretty print
+    new_event.pretty()
 
+# pls ignore this, simply something to remember:
 # for a in soup.findAll('a', title=True):
 #    print "Found this: ", a['title']
